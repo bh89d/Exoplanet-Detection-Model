4474d82b53aff802d22f2ec22f535a8417287ca4
bh89d <bhaumiktrivedi08@gmail.com>
2026-07-20 23:36:14 +0530

    Addition features added to transit and periodicity

--- ml-model/features/periodicity.py
+++ ml-model/features/periodicity.py
@@ -6,7 +6,7 @@ def extract_periodicity_features(data: LightCurveData) -> dict:
   
   time = data.time
   flux = data.flux
-  
+   
   frequency, power = LombScargle(time, flux).autopower()
   
   max_power = max(power)
@@ -19,11 +19,71 @@ def extract_periodicity_features(data: LightCurveData) -> dict:
       best_period = np.inf
   else:
       best_period = 1 / best_frequency
+      
+  phase = (time % best_period) / best_period
   
+  idx = np.argsort(phase)
+  
+  phase = phase[idx]
+  phase_flux = flux[idx]
+  
+  phase_deepest_dip = 1 - np.min(phase_flux)
+  
+  phase_threshold = np.median(phase_flux) - 2*np.std(phase_flux)
+  
+  phase_dip_fraction = np.sum(phase_flux < phase_threshold) / len(phase_flux)
+  
+  phase_mask = phase_flux < phase_threshold
+  
+  max_duration = 0
+  current_duration = 0
+
+  for i in range(len(phase_mask)):
+
+      if phase_mask[i]:
+          current_duration += 1
+
+      else:
+          max_duration = max(
+              max_duration,
+              current_duration
+          )
+
+          current_duration = 0
+
+  phase_max_duration = max(
+      max_duration,
+      current_duration
+  )
+  
+  phase_duration = phase_max_duration * best_period / len(flux)
+  
+  dip = phase_flux[phase_mask]
+  
+  mid = len(dip)//2
+
+  left = dip[:mid]
+
+  right = dip[mid:][::-1]
+
+  n = min(len(left),len(right))
+
+  phase_symmetry = np.mean(np.abs(left[:n]-right[:n]))
+  
+  phase_noise = np.median(np.abs(phase_flux - np.median(phase_flux)))
+  
+  phase_transit_snr = phase_deepest_dip / phase_noise
+
+    
   return {
     "best_period" : best_period,
     "max_power" : max_power,
-    "periodicity_strength" : (max_power - np.mean(power)) / np.std(power) 
+    "periodicity_strength" : (max_power - np.mean(power)) / np.std(power),
+    "phase_deepest_dip" : phase_deepest_dip,
+    "phase_dip_fraction" : phase_dip_fraction,
+    "phase_duration" : phase_duration,
+    "phase_symmetry" : phase_symmetry,
+    "phase_transit_snr" : phase_transit_snr
   }
   
   
--- ml-model/features/transit.py
+++ ml-model/features/transit.py
@@ -8,11 +8,23 @@ def extract_transit_features(data: LightCurveData) -> dict:
   
   dip_segments = []
   
-  current = []
+  dip_time_segments = []
+  
+  dip_centers = []
+  
+  current_flux = []
+  
+  current_time = []
   
   dip_depths = []
   
   dip_durations = []
+  
+  flatness_scores = []
+  
+  ingress_egress_ratios = []
+  
+  dip_areas = []
   
   num_dip_points = 0
   
@@ -23,16 +35,50 @@ def extract_transit_features(data: LightCurveData) -> dict:
   for i in range(len(flux)):
     
     if dip_mask[i]:
-      current.append(flux[i])
+      current_flux.append(flux[i])
+      current_time.append(time[i])
       
     else:
-      if len(current) > 0:
-        dip_segments.append(current)
-        current = []
+      if len(current_flux) > 0:
+        dip_segments.append(current_flux)
+        dip_time_segments.append(current_time)
+        current_flux = []
+        current_time = []
         
-  if len(current) > 0:
-    dip_segments.append(current)
+  if len(current_flux) > 0:
+    dip_segments.append(current_flux)
+  
+  if len(current_time) > 0:
+    dip_time_segments.append(current_time)
     
+  for dip_times in dip_time_segments:
+    dip_centers.append(np.mean(dip_times))
+    
+  num_dip_events = len(dip_segments)
+  
+  if len(dip_centers) >= 2:
+
+      dip_gap = np.diff(dip_centers)
+
+      mean_dip_gap = np.mean(dip_gap)
+      std_dip_gap = np.std(dip_gap)
+
+      dip_gap_cv = (
+          std_dip_gap / mean_dip_gap
+          if mean_dip_gap > 0
+          else 0
+      )
+
+  else:
+      mean_dip_gap = 0
+      std_dip_gap = 0
+      dip_gap_cv = 0
+  
+  if num_dip_events < 2:
+    std_dip_gap = 0
+    dip_gap_cv = 0
+  
+  
   symmetries = []
   
   for dip in dip_segments:
@@ -120,14 +166,11 @@ def extract_transit_features(data: LightCurveData) -> dict:
     mean_dip_depth = 0
   else:
     mean_dip_depth = np.mean(dip_depth)
-  
-  num_dip_events = 0
-  
-  num_dip_events = len(dip_segments)
-  
+
   dip_fraction = significant_dips / len(flux)
   
   max_dip_duration = 0
+  min_dip_duration = np.inf
   current_dip_duration = 0
   
   for i in range(len(dip_mask)):
@@ -137,9 +180,19 @@ def extract_transit_features(data: LightCurveData) -> dict:
     
     else:
       max_dip_duration = max(current_dip_duration, max_dip_duration)
+      min_dip_duration = min(current_dip_duration, min_dip_duration)
       current_dip_duration = 0
       
   max_dip_duration = max(current_dip_duration, max_dip_duration)
+  
+  min_dip_duration = min(current_dip_duration, min_dip_duration)
+  
+  if min_dip_duration == np.inf:
+    min_dip_duration = 0
+  
+  mean_dip_duration = np.mean(dip_durations)
+
+  std_dip_duration = np.std(dip_durations)
   
   duration_days = max_dip_duration * cadence
   
@@ -150,7 +203,7 @@ def extract_transit_features(data: LightCurveData) -> dict:
   if auto_correlation[center] != 0:
     auto_correlation /= auto_correlation[center]
       
-  acf = auto_correlation
+  acf = auto_correlation[len(auto_correlation)//2:]
   
   if len(acf) > 1:
     acf_max = np.max(acf[1:])
@@ -162,6 +215,59 @@ def extract_transit_features(data: LightCurveData) -> dict:
   acf_mean = np.mean(acf)
   
   acf_std = np.std(acf)
+  
+  v_shape_score = deepest_dip / max_dip_duration
+  
+  for dip in dip_segments:
+    
+    dip_area = np.sum(1-np.array(dip))
+    
+    dip_areas.append(dip_area)
+    
+    
+    
+    center = np.argmin(dip)
+    
+    left = dip[:center]
+    
+    right = dip[center + 1:]
+    
+    ingress_duration = len(left)
+    
+    egress_duration = len(right)
+    
+    ingress_egress_ratio = min(ingress_duration, egress_duration) / max(egress_duration, ingress_duration)
+    
+    ingress_egress_ratios.append(ingress_egress_ratio)
+    
+    
+    
+    dip = np.array(dip)
+    
+    depth = 1 - np.min(dip)
+    
+    threshold = np.min(dip) + 0.1*depth
+    
+    flat_region = dip[dip<=threshold]
+    
+    if len(flat_region) > 1 :
+      flatness = np.std(flat_region)
+      
+      flatness_scores.append(flatness)
+      
+  mean_bottom_flatness = np.mean(flatness_scores)
+  
+  mean_ingress_egress_ratio = np.mean(ingress_egress_ratios)
+  
+  min_ingress_egress_ratio = np.min(ingress_egress_ratios)
+  
+  std_ingress_egress_ratio = np.std(ingress_egress_ratios)
+  
+  mean_dip_area = np.mean(dip_areas)
+  
+  max_dip_area = np.max(dip_areas)
+  
+  std_dip_area = np.std(dip_areas)
       
   return {
     "deepest_dip" : deepest_dip,
@@ -169,6 +275,9 @@ def extract_transit_features(data: LightCurveData) -> dict:
     "num_dip_events" : num_dip_events,
     "mean_dip_depth" : mean_dip_depth,
     "max_dip_duration" : max_dip_duration,
+    "min_dip_duration" : min_dip_duration,
+    "mean_dip_duration" : mean_dip_duration,
+    "std_dip_duration" : std_dip_duration,
     "dip_fraction" : dip_fraction,
     "transit_depth_ppm" : deepest_dip * 1e6,
     "mean_symmetry" : mean_symmetry,
@@ -184,5 +293,16 @@ def extract_transit_features(data: LightCurveData) -> dict:
     "acf_max" : acf_max,
     "acf_peak_lag" : acf_peak_lag,
     "acf_mean" : acf_mean,
-    "acf_std" : acf_std
+    "acf_std" : acf_std,
+    "mean_dip_gap" : mean_dip_gap,
+    "std_dip_gap" : std_dip_gap,
+    "dip_gap_cv" : dip_gap_cv,
+    "mean_bottom_flatness" : mean_bottom_flatness,
+    "mean_ingress_egress_ratio" : mean_ingress_egress_ratio,
+    "min_ingress_egress_ratio" : min_ingress_egress_ratio,
+    "std_ingress_egress_ratio" : std_ingress_egress_ratio,
+    "v_shape_score" : v_shape_score,
+    "mean_dip_area" : mean_dip_area,
+    "max_dip_area" : max_dip_area,
+    "std_dip_area" : std_dip_area
   }
