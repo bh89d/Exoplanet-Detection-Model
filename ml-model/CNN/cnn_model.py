97c9fbde8caeaadf0f290a3c449060e4eb20661b
bh89d <bhaumiktrivedi08@gmail.com>
2026-08-01 02:05:40 +0530

    Built dataloaders and prepared data for CNN

--- ml-model/CNN/cnn_model.py
+++ ml-model/CNN/cnn_model.py
@@ -1,7 +1,35 @@
 import numpy as np
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
+import pandas as pd
+from sklearn.model_selection import GroupShuffleSplit
+
+metadata = pd.read_csv("data/ml/features/metadata.csv")
+
+groups = metadata["target_id"]
+
+gss = GroupShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+gss_val = GroupShuffleSplit(n_splits=1, test_size=0.2, random_state= 42)
+
+train_idx , test_idx = next(
+  gss.split(metadata, metadata["label"], groups=groups)
+)
+
+train_df = metadata.iloc[train_idx].reset_index(drop = True)
+test_df = metadata.iloc[test_idx].reset_index(drop = True)
+
+groups = train_df["target_id"]
+
+train_idx , validation_idx = next(
+  gss_val.split(train_df, train_df["label"], groups=groups )
+)
+
+train_split = train_df.iloc[train_idx].reset_index(drop = True)
+validation_df = train_df.iloc[validation_idx].reset_index(drop = True)
+
+train_df = train_split
 
 SEQ_LEN = 4096
 BATCH_SIZE = 64
@@ -9,16 +37,19 @@ EPOCHS = 1000
 
 class LightCurveDataset(Dataset):
   
-  def __init__(self, files, labels):
-    self.files = files
-    self.labels = labels
+  def __init__(self, dataframe):
+    self.data = dataframe
     
   def __len__(self):
-    return len(self.files)
+    return len(self.data)
   
   def __getitem__(self, idx):
     
-    flux = np.load(self.files[idx])
+    row = self.data.iloc[idx]
+    
+    with np.load(row["file_path"]) as curve:
+      
+      flux = curve["flux"]
     
     flux = (flux - np.median(flux))/np.std(flux)
     
@@ -30,26 +61,36 @@ class LightCurveDataset(Dataset):
       
     flux = torch.tensor(flux, dtype=torch.float32).unsqueeze(0)
     
-    label = torch.tensor(self.labels[idx], dtype=torch.float32)
+    label = torch.tensor(row["label"], dtype=torch.float32)
     
     return flux, label
+  
+train_dataset = LightCurveDataset(train_df)
+validation_dataset = LightCurveDataset(validation_df)
+test_dataset = LightCurveDataset(test_df)
 
-class ExoplanetCNN(torch.nn):
+train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+validation_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle= True)
+
+class ExoplanetCNN(nn.Module):
   
   def __init__(self):
-    self.__init__()
+    super().__init__()
     
     self.conv = nn.Sequential(
       nn.Conv1d(1, 16, kernel_size=7, padding=3),
       nn.ReLU(),
       nn.MaxPool1d(2),
       nn.Conv1d(16, 32, kernel_size=5, padding=2),
+      nn.ReLU(),
+      nn.MaxPool1d(2),
       nn.Conv1d(32, 64, kernel_size=5, padding=2),
       nn.ReLU(),
       nn.MaxPool1d(2),
     )
     
-    self.fc == nn.Sequential(
+    self.fc = nn.Sequential(
       nn.Flatten(),
       nn.Linear(64*512, 128),
       nn.ReLU(),
@@ -63,5 +104,21 @@ class ExoplanetCNN(torch.nn):
     x = self.fc(x)
     
     return x.squeeze(1)
-    
-    
+
+exoplanet_cnn_1 = ExoplanetCNN()
+
+device = torch.device("cude" if torch.cuda.is_available() else "cpu")
+
+exoplanet_cnn_1 = exoplanet_cnn_1.to(device)
+
+loss = nn.BCEWithLogitsLoss()
+
+optimizer = torch.optim.Adam(exoplanet_cnn_1.parameters(), lr = 0.001)
+
+x, y = next(iter(train_dataloader))
+
+x = x.to(device)
+
+output = exoplanet_cnn_1(x)
+
+print(output.shape)
